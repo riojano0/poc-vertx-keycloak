f8e4bcf968033a5ab5bd579b5c5d8422729a8220
Daniel.Montivero <montivero.daniel.e@gmail.com>
2022-04-21 12:41:02 -0300

    Create book pojo

---
+++ src/main/java/io/github/riojano0/pocvertxkeycloak/model/Book.java
@@ -0,0 +1,16 @@
+package io.github.riojano0.pocvertxkeycloak.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+
+   private Long id;
+   private String name;
+   private String author;
+
+}
